50202697d2f7e868a1be7e9d498f5d4dc16ac079
Abdul Haris <abdulharis.net.dev@gmail.com>
2018-02-05 18:10:11 +0700

    report

--- report_manajemen/App_Start/BundleConfig.cs
+++ report_manajemen/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace report_manajemen
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css"));
         }
     }
 }
